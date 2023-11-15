411f2ea5985b3543cad3811c22455f2ec6747629
Cameron Edwards <cameron.edwards8@ontariotechu.net>
2023-11-15 17:32:50 -0500

    Cam's sexy html

---
+++ script.js
@@ -0,0 +1,18 @@
+
+  // script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const addBtn = document.getElementById('addItinerary');
+  const editBtn = document.getElementById('editItinerary');
+
+  addBtn.addEventListener('click', function() {
+      console.log('Add itinerary clicked');
+      // Redirect to the add-itinerary.html page when clicked
+      window.location.href = 'add-itinerary.html';
+  });
+
+  editBtn.addEventListener('click', function() {
+      console.log('Edit itinerary clicked');
+      // Code to handle editing an itinerary
+      // (Keep this as it is or modify as needed for edit functionality)
+  });
+});
