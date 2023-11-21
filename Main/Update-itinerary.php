fdb138868a8c25553379b1761f7403f81a9702f3
verinaB <verina.bouls@gmail.com>
2023-11-21 16:41:35 -0500

    added the link from update page to the itinerary-details

--- Main/Update-itinerary.php
+++ Main/Update-itinerary.php
@@ -19,40 +19,63 @@ try {
         $stmt->execute();
 
         echo "Itinerary updated successfully!";
+        // Redirect to a different page after updating the itinerary
+        header("Location: itinerary-details.php");
+        exit(); // Ensure no further output is sent
     }
 
-session_start(); // Start the session
-    if(isset($_SESSION['UserID'])) {
+    session_start(); // Start the session
+    if (isset($_SESSION['UserID'])) {
         $loggedInUserID = $_SESSION['UserID']; // Retrieve the logged-in user's ID from session
 
-        //$itineraryID = 1; // Replace this with the ID of the itinerary to be updated
+        if (isset($_GET['itineraryID'])) {
+            $itineraryID = $_GET['itineraryID'];
 
-        $itineraryID = $_GET['itineraryID'];
-        // Fetch data for the logged-in user's itinerary
-        $stmt = $pdo->prepare("SELECT * FROM itinerary WHERE UserID = :userID AND ItineraryID = :itineraryID");
-        $stmt->bindParam(':userID', $loggedInUserID);
-        $stmt->bindParam(':itineraryID', $itineraryID);
-        $stmt->execute();
-    }
+            // Fetch data for the logged-in user's itinerary
+            $stmt = $pdo->prepare("SELECT * FROM itinerary WHERE UserID = :userID AND ItineraryID = :itineraryID");
+            $stmt->bindParam(':userID', $loggedInUserID);
+            $stmt->bindParam(':itineraryID', $itineraryID);
+            $stmt->execute();
 
-    if ($itinerary) {
-        // Display a form with editable fields for each attribute
-        ?>
-        <form method="POST" action="">
-            <input type="hidden" name="itineraryID" value="<?php echo $itinerary['ItineraryID']; ?>">
-            <label for="startDate">Start Date:</label>
-            <input type="date" name="startDate" value="<?php echo $itinerary['StartDate']; ?>"><br>
+            $itinerary = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            <label for="endDate">End Date:</label>
-            <input type="date" name="endDate" value="<?php echo $itinerary['EndDate']; ?>"><br>
+            if ($itinerary) {
+                // Display a form with editable fields for each attribute
+                ?>
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Add Itinerary</title>
+                    <link rel="stylesheet" href="style-add-itinerary.css">
+                </head>
+                <body>
+                    <div class="nav">
+                        <!-- Your navigation content -->
+                    </div>
+                    <form method="POST" action="">
+                        <input type="hidden" name="itineraryID" value="<?php echo $itinerary['ItineraryID']; ?>">
+                        <label for="startDate">Start Date:</label>
+                        <input type="date" name="startDate" value="<?php echo $itinerary['StartDate']; ?>"><br>
+                        <label for="endDate">End Date:</label>
+                        <input type="date" name="endDate" value="<?php echo $itinerary['EndDate']; ?>"><br>
 
-            <!-- Add other fields as needed -->
+                        <!-- Add other fields as needed -->
 
-            <input type="submit" value="Update Itinerary">
-        </form>
-        <?php
+                        <input type="submit" value="Update Itinerary">
+                    </form>
+                </body>
+                </html>
+                <?php
+            } else {
+                echo "Itinerary not found.";
+            }
+        } else {
+            echo "Itinerary ID not provided.";
+        }
     } else {
-        echo "Itinerary not found.";
+        echo "User not logged in.";
     }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
