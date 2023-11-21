b8e015d20b706ec647086e0872e40efaf8c822e3
Trent <Trent.Jordan@ontariotechu.net>
2023-11-21 15:14:41 -0500

    Revert "Merge branch 'ash' of https://github.com/T-Ginger-J/SOFE3700UFinal into ash"
    This reverts commit 2ff3f1beafc00c76c1e077195e27a5f3a6fcbe2a, reversing
    changes made to c3fa10dfbbba3ad2990faf341313c55b3f727cbc.

--- Main/Update-itinerary.php
+++ Main/Update-itinerary.php
@@ -1,14 +1,12 @@
 <?php
 require_once '../includes/dbh.inc.php';
 
-session_start(); // Start the session
-
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $itineraryID = $_GET['itineraryID'];
+        $itineraryID = $_POST['itineraryID'];
         $startDate = $_POST['startDate'];
         $endDate = $_POST['endDate'];
 
@@ -23,41 +21,33 @@ try {
         echo "Itinerary updated successfully!";
     }
 
-    if(isset($_SESSION['UserID'])) {
-        $loggedInUserID = $_SESSION['UserID']; // Retrieve the logged-in user's ID from session
+    $itineraryID = 1; // Replace this with the ID of the itinerary to be updated
 
-        $itineraryID = 1; // Replace this with the ID of the itinerary to be updated
+    // Fetch data for a specific itinerary
+    $stmt = $pdo->prepare("SELECT * FROM itinerary WHERE UserID = 1 AND ItineraryID = 1;");
+    $stmt->execute();
 
-        // Fetch data for the logged-in user's itinerary
-        $stmt = $pdo->prepare("SELECT * FROM itinerary WHERE UserID = :userID AND ItineraryID = :itineraryID");
-        $stmt->bindParam(':userID', $loggedInUserID);
-        $stmt->bindParam(':itineraryID', $itineraryID);
-        $stmt->execute();
+    // Fetch the itinerary details
+    $itinerary = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Fetch the itinerary details for the logged-in user
-        $itinerary = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($itinerary) {
+        // Display a form with editable fields for each attribute
+        ?>
+        <form method="POST" action="">
+            <input type="hidden" name="itineraryID" value="<?php echo $itinerary['ItineraryID']; ?>">
+            <label for="startDate">Start Date:</label>
+            <input type="date" name="startDate" value="<?php echo $itinerary['StartDate']; ?>"><br>
 
-        if ($itinerary) {
-            // Display the form with editable fields for the logged-in user's itinerary
-            ?>
-            <form method="POST" action="">
-                <input type="hidden" name="itineraryID" value="<?php echo $itinerary['ItineraryID']; ?>">
-                <label for="startDate">Start Date:</label>
-                <input type="date" name="startDate" value="<?php echo $itinerary['StartDate']; ?>"><br>
+            <label for="endDate">End Date:</label>
+            <input type="date" name="endDate" value="<?php echo $itinerary['EndDate']; ?>"><br>
 
-                <label for="endDate">End Date:</label>
-                <input type="date" name="endDate" value="<?php echo $itinerary['EndDate']; ?>"><br>
+            <!-- Add other fields as needed -->
 
-                <!-- Add other fields as needed -->
-
-                <input type="submit" value="Update Itinerary">
-            </form>
-            <?php
-        } else {
-            echo "Itinerary not found for the logged-in user.";
-        }
+            <input type="submit" value="Update Itinerary">
+        </form>
+        <?php
     } else {
-        echo "User not logged in.";
+        echo "Itinerary not found.";
     }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
